dd5754318d37edc0471f4b8f3663dfbce6242f4d
sarthak soni <sarthak.soni@iiitb.org>
2025-05-07 12:48:19 +0530

    Add Cors for frontend support

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
 
 // simple health-check route
 app.get('/api/ping', (_, res) => res.json({ msg: 'pong' }));
