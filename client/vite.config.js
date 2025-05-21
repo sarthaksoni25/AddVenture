ddf49b3551de52746d3eb05fc679cb201ff10716
sarthak soni <sarthak.soni@iiitb.org>
2025-05-21 09:34:15 +0530

    Update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
-// https://vite.dev/config/
+const isDev = process.env.NODE_ENV !== 'production';
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync('../certs/key.pem'),
-      cert: fs.readFileSync('../certs/cert.pem'),
-    },
+    ...(isDev && {
+      https: {
+        key: fs.readFileSync('../certs/key.pem'),
+        cert: fs.readFileSync('../certs/cert.pem'),
+      },
+    }),
     proxy: {
       '/api': 'http://localhost:5000',
     },
