5539e565e2bc24f8a2e2f1ce773d5a60bcd0d166
sarthak soni <sarthak.soni@iiitb.org>
2025-05-21 09:27:37 +0530

    Add certs for dev env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
-console.log('vite config loaded');
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // 👇 add this block
   server: {
+    https: {
+      key: fs.readFileSync('../certs/key.pem'),
+      cert: fs.readFileSync('../certs/cert.pem'),
+    },
     proxy: {
       '/api': 'http://localhost:5000',
     },
