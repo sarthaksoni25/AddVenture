ecdf81820a1f80daca6f4f884d28cb86f8ae8c60
sarthak soni <sarthak.soni@iiitb.org>
2025-05-21 14:32:07 +0530

    Update title

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    ...(isDev && fs.existsSync('../certs/key.pem') && {
-      https: {
-        key: fs.readFileSync('../certs/key.pem'),
-        cert: fs.readFileSync('../certs/cert.pem'),
-      },
+  plugins: [
+    react({
+      fastRefresh: false,
     }),
+  ],
+  server: {
+    ...(isDev &&
+      fs.existsSync("../certs/key.pem") && {
+        https: {
+          key: fs.readFileSync("../certs/key.pem"),
+          cert: fs.readFileSync("../certs/cert.pem"),
+        },
+      }),
     proxy: {
-      '/api': 'http://localhost:5000',
+      "/api": "http://localhost:5000",
     },
   },
 });
